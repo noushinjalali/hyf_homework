df49233668f570b58bab5b02380d5f8fdad02985
noushinjalali <jalali.noushin@gmail.com>
2024-08-13 11:41:13 +0200

    Refactor code to use early return pattern

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,16 +1,18 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-    console.log("No numbers provided. Please provide numbers as arguments.");
-} else {
-    const numbers = args.map(Number);
-
-    if (numbers.some(isNaN)) {
-        console.log("Invalid input. Please provide only numbers.");
-    } else {
-        const sum = numbers.reduce((acc, num) => acc + num, 0);
-        const average = sum / numbers.length;
-
-    console.log(`The average is: ${average}`);
-    }
+  console.log("No numbers provided. Please provide numbers as arguments.");
+  return;
 }
+
+const numbers = args.map(Number);
+
+if (numbers.some(isNaN)) {
+  console.log("Invalid input. Please provide only numbers.");
+  return;
+}
+
+const sum = numbers.reduce((acc, num) => acc + num, 0);
+const average = sum / numbers.length;
+
+console.log(`The average is: ${average}`);
